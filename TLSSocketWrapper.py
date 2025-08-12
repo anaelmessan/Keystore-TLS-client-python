81e083488524bd3e4ebd70c5fbb7169475b41da6
A. M. <a.m@yopmail.com>
2025-08-12 19:12:27 +0200

    Beginning

---
+++ TLSSocketWrapper.py
@@ -0,0 +1,14 @@
+
+
+class TLSSocketWrapper:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def receive(self):
+        pass
+
+    def send(self,data):
+        pass
