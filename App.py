92220fc443ddb534b3fad4291754a8d448d29cea
thanoche <thanushan.pirabakaran@ens.uvsq.fr>
2025-08-15 15:55:54 +0200

    App.py: remove useless comment

--- App.py
+++ App.py
@@ -3,12 +3,9 @@ import TLSSocketWrapper
 import config
 import sys
 
-def read_psk(path):
-    #with open(path, 'r') as file:
-    #    hex_str = file.read().strip()
-
+def read_psk(byte):
     try:
-        byte_data = bytes.fromhex(path)
+        byte_data = bytes.fromhex(byte)
     except ValueError as e:
         print("Invalid hex string:", e)
     return byte_data
@@ -23,6 +20,7 @@ def main():
     # Create server socket instance
     server = TLSSocketWrapper.TLSSocketWrapper(SERVERNAME, hostname=HOST, port=PORT)
     server.set_psk(read_psk(PSK_BYTES))
+    
     # Create CLI interface instance
     cli = CLI_Interface.CLIInterface(server)
 
@@ -38,5 +36,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    #test.write(00, "0102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F20")
 
