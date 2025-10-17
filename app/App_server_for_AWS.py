0f43b99436ff19f4dc6ec16ed6424024fa1e78a9
A. M. <a.m@yopmail.com>
2025-10-17 06:05:41 +0200

    AWS Client communication

---
+++ app/App_server_for_AWS.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+import sys
+from interface.CLI_Interface_AWS_Client import CLIInterfaceAWS
+from core.Controller import Controller
+
+HOST = "0.0.0.0"  # Listen on all interfaces
+DEFAULT_PORT = 6123
+
+# TODO:
+# <...client socket initialization>
+# controller = Controller.Controller(CLIInterfaceServer(client_socket))   #Put this somewhere after each distant client connection
+# #Do this then :
+# while True:
+#     command = input("Enter command: ")  #here retrieve command from socket
+#     status = controller.run_command(command)
+#     if status is False:
+#         break
+
+def transcode_command(command):
+    print("cmd",command)
+    plaintext = ''
+    match command[0]:
+        case 0x00:
+            plaintext += ('read record#' + str(command[2]))
+    return plaintext
+
+def handle_client(conn, addr):
+    print(f"[+] New connection from {addr}")
+    controller = Controller(CLIInterfaceAWS(conn))
+    with conn:
+        while True:
+            #conn.send(b"Enter command:\n")
+            command = conn.recv(1024)
+            status = controller.run_command(transcode_command(command))
+            if status is False:
+                break
+    print(f"[-] Connection closed: {addr}")
+
+
+def main():
+    try:
+        port = int(sys.argv[1])
+    except Exception:
+        port = DEFAULT_PORT
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, port))
+        s.listen()
+        print(f"[*] Listening on {HOST}:{port}...")
+
+        while True:
+            conn, addr = s.accept()
+            # Start a new thread for each client
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.daemon = True  # So threads close when main program ends
+            thread.start()
+
+
+if __name__ == "__main__":
+    main()
